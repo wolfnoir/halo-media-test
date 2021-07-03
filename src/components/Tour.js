aab51d396ed5830c39bce6a476beb2039e1c8f69
StonyBrook3000Game <36480490+wolfnoir@users.noreply.github.com>
2021-07-03 14:24:29 -0400

    Added tour section

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Content.css';
+import './Tour.css';
 
 class Tour extends React.Component {
     render(){
@@ -8,6 +9,84 @@ class Tour extends React.Component {
                 <div className="content-container">
                     <div className="title inverted">
                         Tour
+                    </div>
+                    <div className="tour-flexbox">
+                        <div className="tour-column">
+                            <div className="tour-item">
+                                <div className="tour-details">
+                                    <span className="date">Jun 6</span><br/>
+                                    <span className="venue">Jewel Nightclub</span><br/>
+                                    <span className="city">Las Vegas, NV</span>
+                                </div>
+                                <div className="tour-links">
+                                    <a href="#">Tickets</a>
+                                    <a href="#">VIP</a>
+                                    <a href="#">RSVP</a>
+                                </div>
+                            </div>
+                            <div className="tour-item">
+                                <div className="tour-details">
+                                    <span className="date">Aug 7</span><br/>
+                                    <span className="venue">Jewel Nightclub</span><br/>
+                                    <span className="city">Las Vegas, NV</span>
+                                </div>
+                                <div className="tour-links">
+                                    <a href="#">Tickets</a>
+                                    <a href="#">VIP</a>
+                                    <a href="#">RSVP</a>
+                                </div>
+                            </div>
+                            <div className="tour-item">
+                                <div className="tour-details">
+                                    <span className="date">Jun 31</span><br/>
+                                    <span className="venue">Jewel Nightclub</span><br/>
+                                    <span className="city">Las Vegas, NV</span>
+                                </div>
+                                <div className="tour-links">
+                                    <a href="#">Tickets</a>
+                                    <a href="#">VIP</a>
+                                    <a href="#">RSVP</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tour-column">
+                        <div className="tour-item">
+                                <div className="tour-details">
+                                    <span className="date">Jun 6</span><br/>
+                                    <span className="venue">Jewel Nightclub</span><br/>
+                                    <span className="city">Las Vegas, NV</span>
+                                </div>
+                                <div className="tour-links">
+                                    <a href="#">Tickets</a>
+                                    <a href="#">VIP</a>
+                                    <a href="#">RSVP</a>
+                                </div>
+                            </div>
+                            <div className="tour-item">
+                                <div className="tour-details">
+                                    <span className="date">Jun 6</span><br/>
+                                    <span className="venue">Jewel Nightclub</span><br/>
+                                    <span className="city">Las Vegas, NV</span>
+                                </div>
+                                <div className="tour-links">
+                                    <a href="#">Tickets</a>
+                                    <a href="#">VIP</a>
+                                    <a href="#">RSVP</a>
+                                </div>
+                            </div>
+                            <div className="tour-item">
+                                <div className="tour-details">
+                                    <span className="date">Jun 31</span><br/>
+                                    <span className="venue">Jewel Nightclub</span><br/>
+                                    <span className="city">Las Vegas, NV</span>
+                                </div>
+                                <div className="tour-links">
+                                    <a href="#">Tickets</a>
+                                    <a href="#">VIP</a>
+                                    <a href="#">RSVP</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="button-right">
                         <button className="releases-button button yellow">View All Dates</button>
