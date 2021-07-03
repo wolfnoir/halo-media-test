bfcceef43cadc9204c06da1a2a8d49e8bf315da9
StonyBrook3000Game <36480490+wolfnoir@users.noreply.github.com>
2021-07-03 17:17:11 -0400

    Minor css tweaks

--- src/components/Store.js
+++ src/components/Store.js
@@ -6,36 +6,36 @@ import store2 from "../images/BananaSplitShort.png";
 import store3 from "../images/BananaSplitDigitalSingle.png";
 
 class Store extends React.Component {
-    render(){
-        return(
-                <div className="content-container yellow">
-                    <div className="title inverted black">
-                        Store
+    render() {
+        return (
+            <div className="content-container yellow">
+                <div className="title inverted black">
+                    Store
+                </div>
+                <div className="store-flexbox">
+                    <div className="store-item">
+                        <div className="store-image" style={{ backgroundImage: `url(${store1})` }} />
+                        <span className="name">Banana Split Long Sleeve</span><br />
+                        <span className="price">$30.00</span>
                     </div>
-                    <div className="store-flexbox">
-                        <div className="store-item">
-                            <div className="store-image" style={{backgroundImage: `url(${store1})`}}/>
-                            <span className="name">Banana Split Long Sleeve</span><br/>
-                            <span className="price">$30.00</span>
-                        </div>
-                        <div className="store-item">
-                            <div className="store-image" style={{backgroundImage: `url(${store2})`}}/>
-                            <span className="name">Banana Split Short Sleeve</span><br/>
-                            <span className="price">$25.00</span>
-                        </div>
-                        <div className="store-item">
-                            <div className="store-image" style={{backgroundImage: `url(${store3})`}}/>
-                            <span className="name">Banana Split - Digital Single</span><br/>
-                            <span className="price">$1.29</span>
-                        </div>
+                    <div className="store-item">
+                        <div className="store-image" style={{ backgroundImage: `url(${store2})` }} />
+                        <span className="name">Banana Split Short Sleeve</span><br />
+                        <span className="price">$25.00</span>
                     </div>
-                    <div className="button-div right">
-                        <a href="#" className="releases-button button black">Shop All</a>
+                    <div className="store-item">
+                        <div className="store-image" style={{ backgroundImage: `url(${store3})` }} />
+                        <span className="name">Banana Split - Digital Single</span><br />
+                        <span className="price">$1.29</span>
                     </div>
                 </div>
+                <div className="button-div right">
+                    <a href="#" className="releases-button button black">Shop All</a>
+                </div>
+            </div>
         );
     }
-    
+
 }
 
 export default Store
