737e4e8bb2a0be6b89b0ce8f07baefe9c9070790
StonyBrook3000Game <36480490+wolfnoir@users.noreply.github.com>
2021-07-04 15:55:11 -0400

    Added videos section

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,14 +1,97 @@
 import React from 'react';
 import './Content.css';
+import './Videos.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+const handleDragStart = (e) => e.preventDefault();
+
+const responsive = {
+    800: { items: 1 },
+    1024: { items: 2 },
+};
+
+const VIDEO_WIDTH = 800, VIDEO_HEIGHT = 450;
+
+const items = [
+    <div style={{maxWidth: "80%"}}>
+        <iframe src="https://www.youtube.com/embed/pNmXEL6g6xo" title="YouTube video player" frameborder="0" onDragStart={handleDragStart} 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <div className="video-details">
+            <div className="title">Banana Split</div>
+            <div className="subtitle">MURDA BEATZ (FEAT. YNW MELLY &amp; LIL DURK)</div>
+        </div>
+    </div>,
+    <div style={{maxWidth: "80%"}}>
+        <iframe src="https://www.youtube.com/embed/KBXsx1ZHgg0" title="YouTube video player" frameborder="0" onDragStart={handleDragStart} 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <div className="video-details">
+            <div className="title">Banana Split</div>
+            <div className="subtitle">MURDA BEATZ (FEAT. YNW MELLY &amp; LIL DURK)</div>
+        </div>
+    </div>,
+    <div style={{maxWidth: "80%"}}>
+        <iframe src="https://www.youtube.com/embed/n5O3rLeszVY" title="YouTube video player" frameborder="0" onDragStart={handleDragStart} 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <div className="video-details">
+            <div className="title">Banana Split</div>
+            <div className="subtitle">MURDA BEATZ (FEAT. YNW MELLY &amp; LIL DURK)</div>
+        </div>
+    </div>,
+];
 
 class Videos extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeIndex: 0
+        };
+    }
+
+    slidePrev = () => {
+        this.setState({
+            activeIndex: this.state.activeIndex - 1
+        })
+    }
+
+    slideNext = () => {
+        this.setState({
+            activeIndex: this.state.activeIndex + 1
+        })
+    }
+
+    syncActiveIndex =({item}) => {
+        this.setState({
+            activeIndex: item
+        })
+    }
+
     render() {
+
         return (
             <div className="content-container gray">
                 <div className="title inverted">
                     Videos
                 </div>
+                <div className="video-carousel">
+                    <center>
+                    <AliceCarousel
+                        mouseTracking
+                        disableDotsControls
+                        disableButtonsControls
+                        items={items}
+                        activeIndex = {this.state.activeIndex}
+                        responsive={responsive}
+                        keyboardNavigation = {true}
+                        onSlideChanged={this.syncActiveIndex}
+                        infinite={true} />
+                    </center>
+                </div>
                 <div className="button-div right">
+                    <div className="video-buttons">
+                        <button onClick={this.slidePrev}>◀</button>
+                        <button onClick={this.slideNext}>▶</button>
+                    </div>
                     <a href="#" className="button yellow">View All Videos</a>
                 </div>
             </div>
